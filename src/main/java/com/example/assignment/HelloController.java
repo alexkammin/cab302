f0fe7c603b9527288d0ca114c13b0cae06fd094e
jack-vickers <jackvickers@optusnet.com.au>
2026-08-15 18:12:03 +1000

    Made draft account creation page

--- src/main/java/com/example/assignment/HelloController.java
+++ src/main/java/com/example/assignment/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.assignment;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onCreateAccount() {
     }
 }
