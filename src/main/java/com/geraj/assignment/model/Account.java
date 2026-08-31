8e171646f8b6e6e29870579bd796e5c7e0825544
jack-vickers <jackvickers@optusnet.com.au>
2026-08-31 17:19:04 +1000

    Merged User class with old Account class

--- src/main/java/com/geraj/assignment/model/Account.java
+++ src/main/java/com/geraj/assignment/model/Account.java
@@ -1,27 +1,39 @@
 package com.geraj.assignment.model;
 
-public class Account {
-    private String username;
-    private String password;
+import java.util.Objects;
 
-    public Account(String username, String password) {
-        this.username = username;
-        this.password = password;
+public class User {
+    private String name;
+    private String email;
+    private String hash;
+
+    public User(String name, String email, String hash) {
+        this.name = Objects.requireNonNull(name, "Name cannot be null");
+        this.email = Objects.requireNonNull(email, "Email cannot be null");
+        this.hash = Objects.requireNonNull(hash, "Hash cannot be null");
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return this.name;
     }
 
-    public String getPassword() {
-        return password;
+    public void setName(String name) {
+        this.name = Objects.requireNonNull(name, "Name cannot be null");
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public String getEmail() {
+        return this.email;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setEmail(String email) {
+        this.email = Objects.requireNonNull(email, "Email cannot be null");
+    }
+
+    public String getHash() {
+        return this.hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = Objects.requireNonNull(hash, "Hash cannot be null");
     }
 }
