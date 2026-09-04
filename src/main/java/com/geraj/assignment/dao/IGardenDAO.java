33ac5b96a068b45e8701424baa57a4d58cde29e0
jack-vickers <jackvickers@optusnet.com.au>
2026-09-04 16:07:47 +1000

    Worked on Garden and changed some error messages in Account

--- src/main/java/com/geraj/assignment/dao/IGardenDAO.java
+++ src/main/java/com/geraj/assignment/dao/IGardenDAO.java
@@ -1,4 +1,7 @@
 package com.geraj.assignment.dao;
 
 public interface IGardenDAO {
+    void createGarden(String name, String hash);
+
+    void findGarden(String name, String Location);
 }
---
+++ src/main/java/com/geraj/assignment/model/Garden.java
@@ -0,0 +1,4 @@
+package com.geraj.assignment.model;
+
+public class Garden {
+}
