5ba4bcbde25b43aa77efcd4a38caf91e28d14990
jack-vickers <jackvickers@optusnet.com.au>
2026-09-04 17:58:26 +1000

    added findGardens

--- src/main/java/com/geraj/assignment/dao/IGardenDAO.java
+++ src/main/java/com/geraj/assignment/dao/IGardenDAO.java
@@ -2,8 +2,10 @@ package com.geraj.assignment.dao;
 
 import com.geraj.assignment.model.Garden;
 
+import java.util.ArrayList;
+
 public interface IGardenDAO {
     void createGarden(Garden garden);
 
-//    Garden findGarden(String name, String location);
+    ArrayList<Garden> findGardens(String searchName, String searchLocation);
 }
--- src/main/java/com/geraj/assignment/dao/SqliteGardenDAO.java
+++ src/main/java/com/geraj/assignment/dao/SqliteGardenDAO.java
@@ -1,10 +1,13 @@
 package com.geraj.assignment.dao;
 
+import com.geraj.assignment.model.Account;
 import com.geraj.assignment.model.Garden;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class SqliteGardenDAO implements IGardenDAO {
     private final Connection connection;
@@ -23,7 +26,7 @@ public class SqliteGardenDAO implements IGardenDAO {
                 temperature REAL,
                 precipitation REAL,
                 atmosphericHumidity REAL,
-                owner_name INTEGER,
+                owner_name TEXT,
                 FOREIGN KEY (owner_name) REFERENCES accounts(name)
             );
             """;
@@ -71,8 +74,73 @@ public class SqliteGardenDAO implements IGardenDAO {
             }
 
             statement.executeUpdate();
+
+//            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+//                if (generatedKeys.next()) {
+//                    garden.setId(generatedKeys.getInt(1));
+//                }
+//            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public ArrayList<Garden> findGardens(String searchName, String searchLocation) {
+        ArrayList<Garden> gardens = new ArrayList<>();
+
+        String query = """
+            SELECT *
+            FROM gardens
+            WHERE (? IS NULL OR name LIKE ?)
+              AND (? IS NULL OR location LIKE ?)
+            """;
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            if (searchName != null && !searchName.isBlank()) {
+                statement.setString(1, searchName);
+                statement.setString(2, "%" + searchName + "%");
+            } else {
+                statement.setNull(1, java.sql.Types.VARCHAR);
+                statement.setNull(2, java.sql.Types.VARCHAR);
+            }
+
+            if (searchLocation != null && !searchLocation.isBlank()) {
+                statement.setString(3, searchLocation);
+                statement.setString(4, "%" + searchLocation + "%");
+            } else {
+                statement.setNull(3, java.sql.Types.VARCHAR);
+                statement.setNull(4, java.sql.Types.VARCHAR);
+            }
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    String name = resultSet.getString("name");
+                    String location = resultSet.getString("location");
+
+                    Double temperature = resultSet.getDouble("temperature");
+                    if (resultSet.wasNull()) temperature = null;
+
+                    Double precipitation = resultSet.getDouble("precipitation");
+                    if (resultSet.wasNull()) precipitation = null;
+
+                    Integer atmosphericHumidity = resultSet.getInt("atmosphericHumidity");
+                    if (resultSet.wasNull()) atmosphericHumidity = null;
+
+                    String ownerName = resultSet.getString("owner_name");
+                    Account owner = null;
+                    if (ownerName != null) {
+                        IAccountDAO accountDAO = new SqliteAccountDAO();
+                        owner = accountDAO.getAccountByName(ownerName);
+                    }
+
+                    Garden garden = new Garden(name, location, temperature, precipitation, atmosphericHumidity, owner);
+                    gardens.add(garden);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return gardens;
     }
 }
