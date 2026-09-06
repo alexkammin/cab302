13e9c650bc84f6ff1ea3072c9cde07445c70ae2a
jack-vickers <jackvickers@optusnet.com.au>
2026-09-06 18:26:07 +1000

    Added button to sign in page which redirects to main page - sign in functionality not present

--- src/main/java/com/geraj/assignment/controller/SignInController.java
+++ src/main/java/com/geraj/assignment/controller/SignInController.java
@@ -1,4 +1,20 @@
 package com.geraj.assignment.controller;
 
+import com.geraj.assignment.PasswordService;
+import com.geraj.assignment.SceneSwitcher;
+import com.geraj.assignment.dao.IAccountDAO;
+import com.geraj.assignment.dao.SqliteAccountDAO;
+import com.geraj.assignment.model.Account;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SignInController {
+
+    @FXML private Button signInButton;
+
+    @FXML
+    private void onSignIn(ActionEvent actionEvent) {
+        SceneSwitcher.switchScene(actionEvent, "main-view.fxml");
+    }
 }
