a29c4d13246ba42f2ab9ec0bb2cbe5e1d14ee00c
jack-vickers <jackvickers@optusnet.com.au>
2026-09-10 21:28:49 +1000

    Added main button to nav bar

--- src/main/java/com/geraj/assignment/controller/NavController.java
+++ src/main/java/com/geraj/assignment/controller/NavController.java
@@ -30,4 +30,12 @@ public class NavController {
                 "account-profile-view.fxml"
         );
     }
+
+    @FXML
+    private void goToMain(MouseEvent actionEvent) {
+        SceneSwitcher.switchScene(
+                actionEvent,
+                "main-view.fxml"
+        );
+    }
 }
