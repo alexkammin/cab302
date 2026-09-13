81963e16f8ed2b3684aa8df4a98589d778dcb2bf
jack-vickers <jackvickers@optusnet.com.au>
2026-09-13 19:23:40 +1000

    Made garden list contain string rather than memory address

--- src/main/java/com/geraj/assignment/controller/GardenEditorController.java
+++ src/main/java/com/geraj/assignment/controller/GardenEditorController.java
@@ -5,12 +5,24 @@ import com.geraj.assignment.model.Garden;
 import com.geraj.assignment.model.GardenPlot;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.util.ArrayList;
 
 public class GardenEditorController {
     private Garden current_garden;
 
+    @FXML private ListView<GardenPlot> plotListView;
+
     public void setGarden(Garden garden) {
         current_garden = garden;
+        plotListView.getItems().addAll(current_garden.getGardenPlots());
+
+        plotListView.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2 && plotListView.getSelectionModel().getSelectedItem() != null) {
+                onClickGardenPlot(new ActionEvent(plotListView, null));
+            }
+        });
     }
 
     @FXML
@@ -26,7 +38,15 @@ public class GardenEditorController {
 
             if (newPlot != null) {
                 current_garden.addGardenPlot(newPlot);
+
+                plotListView.getItems().add(newPlot);
             }
         }
+    }
+
+    @FXML
+    private void onClickGardenPlot(ActionEvent actionEvent) {
+        GardenPlot selectedGarden = plotListView.getSelectionModel().getSelectedItem();
+
     }
 }
